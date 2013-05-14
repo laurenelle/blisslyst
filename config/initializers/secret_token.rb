c46b7ae6a36c94b3a41f9e3cd785d33213716033
Steve Chen <steve.chen@couchsurfing.com>
2013-05-13 22:44:46 -0700

    initial rails project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blisslyst::Application.config.secret_token = '49a877f7aad2d2738f574cf6d6700ce44b9db221a460f48d5fcf9553ff24827e3c5c6e7ee150e72e7644a5be9e84bcdf6790fc6c2c2753b725eb79e47bbab07d'
